039978d56df585f7fdb22c381f36b952b7504260
nlagdhir <nlagdhir@gmail.com>
2023-06-15 20:14:42 +0600

    getting video info

--- index.js
+++ index.js
@@ -7,6 +7,39 @@ const compression = require("compression");
 
 app.use(cors());
 
+app.use(compression());
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }));
+
+app.use(express.json({ limit: "10mb" }));
+
+app.post("/api/get-info", async (req, res) => {
+  const { videoUrl } = req.body;
+
+  try {
+    const videoId = ytdl.getURLVideoID(videoUrl);
+
+    if (!videoId) {
+      res.status(400).send({ error: "Invalid video URL" });
+      return;
+    }
+
+    const informatiion = await ytdl.getInfo(videoId);
+    const title = informatiion.videoDetails.title;
+    const thumbnail = informatiion.videoDetails.thumbnails[0].url;
+    const uploadDate = informatiion.videoDetails.uploadDate;
+
+    const metaInfo = await ytdl.getInfo(videoUrl);
+    let formateData = {
+      url: "https://www.youtube.com/embed/" + videoId,
+      info: metaInfo.formats,
+    };
+    const data = { title, thumbnail, uploadDate, videoUrl, formateData };
+    res.send({ success: true, data });
+  } catch (error) {
+    res.status(500).send({ error: "Failed to process the video URL" });
+  }
+});
 
 const port = 4000;
 app.get("/", (req, res) => res.send("Hello World!"));
